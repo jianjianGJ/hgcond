b67d2e716fd95f5634217dfcaaf589dcbab31edd
jianjianGJ <1397919138@qq.com>
2023-11-06 21:45:14 +0800

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+from .HeteroSGC import HeteroSGC
+from .HeteroGCN import HeteroGCN
+from .HeteroSAGE import HeteroSAGE
+from .HAN import HAN
+from .HeteroGCN2 import HeteroGCN2
+
+__all__ = [
+    'HeteroSGC',
+    'HeteroGCN',
+    'HeteroSAGE',
+    'HAN',
+    'HeteroGCN2',
+]
